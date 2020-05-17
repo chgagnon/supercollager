d8c358555b08d50d0f2988c1785c9d6a82a01f38
Charlie <charlie@Charlies-MacBook-Air.local>
2020-05-17 19:29:48 -0400

    It's alive

---
+++ load_components.py
@@ -0,0 +1,88 @@
+import argparse
+import numpy as np
+import os
+import glob
+from skimage import io, img_as_ubyte
+from skimage.transform import resize
+import matplotlib.pyplot as plt
+from shutil import rmtree
+
+'''
+Determines average color of each image that will compose the collage
+@param: components - (num_images, components_width, components_height, 3) array
+
+@return: (num_images, 3) array - an RGB color vector for each image
+'''
+def get_average_colors(components):
+  return np.mean(components, (1,2))
+
+def get_components(components_path, components_height, components_width, refresh_components):
+  
+  save_resized_path = os.path.join(components_path, 'resized')
+
+
+  if refresh_components:
+    rmtree(save_resized_path)
+
+  if (not os.path.exists(save_resized_path)):
+    
+    os.mkdir(save_resized_path)
+
+    list_component_images = glob.glob(os.path.join(components_path, '*.*'))
+
+    components = [] 
+
+    for img_num, filename in enumerate(list_component_images):
+      img = io.imread(filename)
+      print('Input is', img.shape, filename)
+
+      # square off the images and resize to proper width and height
+
+      input_width = img.shape[1]
+      input_height = img.shape[0]
+
+      crop_margin = np.abs(input_height - input_width)
+      left_crop = np.floor(crop_margin / 2).astype(int)
+      right_crop = None
+      if crop_margin % 2 != 0:
+        right_crop = left_crop + 1
+      else:
+        right_crop = left_crop
+
+      print(right_crop)
+
+      cropped_img = None
+
+      if input_height > input_width:
+        cropped_img = img[left_crop : -right_crop, :, :]
+      else:
+        cropped_img = img[:, left_crop:-right_crop, :]
+
+      assert(cropped_img is not None)
+
+      print('Cropped to', cropped_img.shape)
+
+      final_img = resize(cropped_img, (components_width, components_height), anti_aliasing=True)
+      
+      final_img = img_as_ubyte(final_img)
+
+      components.append(final_img)
+      # io.imshow(final_img)
+
+      io.imsave(os.path.join(save_resized_path, str(img_num) + '.png'), final_img)
+      plt.show()
+
+    return components, get_average_colors(components)
+
+  else: 
+    # load components from components directory
+    components = []
+    list_component_images = glob.glob(os.path.join(save_resized_path, '*.*'))
+
+    for filename in list_component_images:
+      image = io.imread(filename)
+      print(image.shape)
+      components.append(image)
+    components = np.array(components)
+
+    return components, get_average_colors(components)
--- makemosaic.py
+++ makemosaic.py
@@ -2,90 +2,102 @@ import argparse
 import numpy as np
 import os
 import glob
-from skimage import io
-from skimage.transform import resize, rotate
+from skimage import io, img_as_ubyte
 import matplotlib.pyplot as plt
-from shutil import rmtree
+from load_components import get_components
 
-parser = argparse.ArgumentParser(description='Specify preprocessing mode')
+parser = argparse.ArgumentParser(description='Specify whether to regenerate component images')
 
 parser.add_argument('--refresh_components', action='store_true')
 
 args = parser.parse_args()
 
 components_path = os.path.join('images', 'components')
-save_resized_path = os.path.join(components_path, 'resized')
+
+# Right now, the shape of components is square
+# Changing this will require a different approach to cropping
+components_width = 20
+components_height = 20
 
 targets_path = os.path.join('images', 'targets')
+output_path = os.path.join('images', 'mosaics')
 
-# Right now, the shape of components is sqauare
-# Changing this will require a different approach to cropping
-components_width = 100
-components_height = 100
+RESOLUTION_MULTIPLIER = 4
 
-# -----------------------------
+'''
+Determine which component image has nearest average color to current target
+image region
 
-if args.refresh_components:
-  rmtree(save_resized_path)
+@param: target_tile_color - (3,) array of current target region's average RGB
+color
 
-if (not os.path.exists(save_resized_path)):
-  
-  os.mkdir(save_resized_path)
+@param: component_colors - (num_components, 3) array of each component's
+average RGB vector
 
-  list_component_images = glob.glob(os.path.join(components_path, '*.*'))
+@return: index of component image with nearest average color (TODO later: optional modulation
+towards average color of corresponding target image region)
+'''
+def get_nearest_component(target_tile_color, component_colors):
+  # Taking Euclidean distance
+  color_distances = np.sum((target_tile_color - component_colors)**2, axis=1)
+  nearest_image_index = np.argmin(color_distances)
 
-  components = []
-
-  for img_num, filename in enumerate(list_component_images):
-    img = io.imread(filename)
-    print('Input is', img.shape, filename)
-
-    # square off the images and resize to proper width and height
-
-    input_width = img.shape[1]
-    input_height = img.shape[0]
+  return nearest_image_index
 
 
-    crop_margin = np.abs(input_height - input_width)
-    left_crop = np.floor(crop_margin / 2).astype(int)
-    right_crop = None
-    if crop_margin % 2 != 0:
-      right_crop = left_crop + 1
-    else:
-      right_crop = left_crop
+def main():
 
-    print(right_crop)
+  components, component_colors = get_components(components_path, components_width, 
+                              components_height, args.refresh_components)
+
+  print('Components loaded')
+
+  target_files = glob.glob(os.path.join(targets_path, '*.*'))
+
+  for target_filename in target_files:
+
+    target_img = io.imread(target_filename)
+
+    print('Target has shape:', target_img.shape)
+
+    # Excess portions of the target, in the bottom-right, will be clipped off
+    # The generated image will have 4x the resolution of the original target
+        # (e.g. replacing 50-pix squares with 100-pix squares)
+    height_in_tiles = np.floor(RESOLUTION_MULTIPLIER * target_img.shape[0] / components_height).astype(int)
+    width_in_tiles = np.floor(RESOLUTION_MULTIPLIER * target_img.shape[1] / components_width).astype(int)
+
+    print(height_in_tiles)
+    print(width_in_tiles)
+
+    mosaic = np.empty((height_in_tiles*components_height, width_in_tiles*components_width, 3))
+
+    for i in range(height_in_tiles):
+      for j in range(width_in_tiles):
+        # target_tile_color is (3,) array - average R, G, B of corresponding
+        # tile in the target
+        # print("Now preparing tile at (" + str(i) + ", " + str(j) + ")" )
+
+        height_stride_on_target = int(components_height / RESOLUTION_MULTIPLIER)
+        width_stride_on_target = int(components_width / RESOLUTION_MULTIPLIER)
 
 
-    cropped_img = None
+        target_tile_color = np.mean(
+          target_img[i*height_stride_on_target:int((i+1)*height_stride_on_target),
+                    j*width_stride_on_target:int((j+1)*width_stride_on_target)], (0,1))
 
-    if input_height > input_width:
-      cropped_img = img[left_crop : -right_crop, :, :]
-    else:
-      cropped_img = img[:, left_crop:-right_crop, :]
+        comp_index = get_nearest_component(target_tile_color, component_colors)
 
-    assert(cropped_img is not None)
+        mosaic[i*components_height:(i+1)*components_height,
+              j*components_width:(j+1)*components_width] = components[comp_index]
+        print("added tile of shape", components[comp_index].shape)
 
-    print('Cropped to', cropped_img.shape)
+    mosaic = mosaic.astype(np.uint8)
 
-    final_img = resize(cropped_img, (components_width, components_height), anti_aliasing=True)
-    
-    components.append(final_img)
-    # io.imshow(final_img)
+    print(mosaic)
 
-    io.imsave(os.path.join(save_resized_path, str(img_num) + '.png'), final_img.astype(np.uint8))
+    io.imsave(os.path.join(output_path, os.path.basename(target_filename) + '_mosaic.png'), mosaic)
+    io.imshow(mosaic)
     plt.show()
 
-# components already resized appropriately
-else: 
-
-  print('cool')
-
-  list_target_images = glob.glob(os.path.join(targets_path, '*.*'))
-
-  for target in list_target_images:
-
-
-
-
-# print(np.array(components))
+if __name__ == '__main__':
+  main()
